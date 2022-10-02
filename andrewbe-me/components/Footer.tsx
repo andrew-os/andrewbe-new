7810f0a5705a479f32791820031945eb173bbf96
Andrew Sleigh <andrew.sleighweb@gmail.com>
2022-10-02 16:08:34 +1300

    fix hydrate bug

--- andrewbe-me/components/Footer.tsx
+++ andrewbe-me/components/Footer.tsx
@@ -3,30 +3,28 @@ import YearToday from './YearToday'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedinIn, faGithubAlt } from '@fortawesome/free-brands-svg-icons'
 
-interface FooterProps{
-    children?: React.ReactNode|React.ReactNode[];
-}
-export default function Footer({}: FooterProps){
+
+export default function Footer(){
     return(
-        <footer className=" flex flex-row justify-end">
+        <footer className="flex flex-row justify-end">
           <div className='md:w-3/12 bg-brand-slate py-4 px-5'>
             <ul className="flex flex-col">
               <ul className="flex flex-row">
                 <li className="mr-4">
-                  <a className='text-white' href="https://www.linkedin.com/in/asleigh/" target={`_blank`}>
+                  <a className='text-white' href="https://www.linkedin.com/in/asleigh/" target="_blank" rel="noreferrer">
                     <FontAwesomeIcon icon={faLinkedinIn} className="text-white hover:text-primary" />
                   </a>
                 </li>
                 <li>
-                <a className='text-white' target={`_blank`} href="https://github.com/andrew-os">
+                <a className='text-white' target="_blank" href="https://github.com/andrew-os" rel="noreferrer">
                   <FontAwesomeIcon icon={faGithubAlt} className="text-white hover:text-primary" />
                   </a>
                 </li>
               </ul>
             <li>
-              <a className='text-white hover:text-primary'>
-                <Link href="/play">about me</Link>
-              </a>
+              <Link href="/play">
+                <a className='text-white hover:text-primary'>about me</a>
+              </Link>
             </li>
             <li className='text-white'>&copy; <YearToday /> andrewbeme</li>
 
--- andrewbe-me/components/Header.tsx
+++ andrewbe-me/components/Header.tsx
@@ -8,13 +8,13 @@ export default function Header({isHomePage = false}: HeaderProps){
   return (
     <header className={`${isHomePage ? `bg-primary` : `bg-white`} shadow-md relative`}>
       <MainNav invertColors={isHomePage} />
-      {/* {isHomePage ?
+      {isHomePage ?
         (
           null
         ) : (
           <div className="gradient-line absolute bottom-0 w-full h-4"></div>
         )
-    } */}
+    }
 
     </header>
     )
