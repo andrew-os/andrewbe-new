ef84fdadb639e9fbe898cb2801385940ce5b79da
Andrew Sleigh <andrew.sleighweb@gmail.com>
2022-10-07 18:48:24 +1300

    fix types on link button to remove build fail cause of any type

--- andrewbe-me/components/Button.tsx
+++ andrewbe-me/components/Button.tsx
@@ -2,24 +2,27 @@ import Link from "next/link"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
-interface ButtonProps{
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+interface ButtonStaticProps{
   text: string
   hasArrow?: boolean
   arrowColor?: string
   secondaryColor?: boolean
   borderColor?: string
   classes?: string
-  isLink: boolean
   href?: string
+}
+
+interface ButtonProps extends ButtonStaticProps{
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  isLink: boolean
 
 }
 
-function LinkButton({href, title, hasArrow, classes, arrowColor, borderColor}){
+function LinkButton({text, href, hasArrow, classes, arrowColor, borderColor}: ButtonStaticProps){
   return (
     <Link href={`/${href}`}>
       <a className={`${classes}`}>
-        <span className={`inline border-b-3 border-primary pb-1 ${borderColor}`}>{title}</span>
+        <span className={`inline border-b-3 border-primary pb-1 ${borderColor}`}>{text}</span>
       {hasArrow ? (
         <span className="ml-4"><FontAwesomeIcon icon={faArrowRight } className={`${arrowColor}`} /></span>
       ) : (
@@ -34,7 +37,7 @@ function LinkButton({href, title, hasArrow, classes, arrowColor, borderColor}){
 export default function Button({isLink, onClick, text, href, classes, hasArrow, arrowColor, borderColor="border-primary", secondaryColor = false }: ButtonProps) {
   return (
     isLink ? (
-      <LinkButton href={href} title={text} hasArrow={hasArrow} classes={classes} arrowColor={arrowColor} borderColor={borderColor} />
+      <LinkButton href={href} text={text} hasArrow={hasArrow} classes={classes} arrowColor={arrowColor} borderColor={borderColor} />
     ) :(
     <button className={`${classes} ${secondaryColor ? `bg-secondary` : `bg-primary`}`} onClick={onClick}>
       {text}
