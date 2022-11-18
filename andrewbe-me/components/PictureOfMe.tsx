521aa6468c153519c83bace40c5e49911ad9d03d
Andrew Sleigh <andrew.sleighweb@gmail.com>
2022-11-18 12:48:57 +0000

    second fix on image path

--- andrewbe-me/components/PictureOfMe.tsx
+++ andrewbe-me/components/PictureOfMe.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import me from '../public/orange-me-final--crop.png'
 
 export default function PictureOfMe() {
   return (
     <div className="md:col-start-6 md:col-span-7  relative">
       <div className="flex justify-end">
-        <Image src="/../public/orange-me-final--crop.png" alt="me" width="520" height="566" />
+        <Image src={me} alt="me" width="520" height="566" />
       </div>
     </div>
   )
